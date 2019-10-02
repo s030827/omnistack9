0e0d541a8af8e04d7da5a5f78f21330ecef22751
Marcus Almeida <s030827@gmail.com>
2019-10-02 12:26:50 -0300

    Connect database on Mongodb Atlas

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
-const express = require('express');
-const routes  = require('./routes')
 // import express from 'express';
+const express = require('express');
+const mongoose = require('mongoose');
+const routes  = require('./routes')
+require('dotenv').config()
 
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@omnistack-ixxc3.mongodb.net/omnistack9_dev?retryWrites=true&w=majority`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+  
 const app = express();
 app.use(express.json());
 
