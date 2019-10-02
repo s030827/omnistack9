ff2c670c04fcf7a94538b2ae7545a4e9790b9871
Marcus Almeida <s030827@gmail.com>
2019-10-01 21:51:22 -0300

    Create  basic config for routes

--- src/server.js
+++ src/server.js
@@ -2,9 +2,24 @@ const express = require('express')
 // import express from 'express';
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (_req, _res) => {
-  return _res.json('{ message: "Hello Word" }');
+app.get('/users', (_req, _res) => {
+  return _res.json({ message: "Hello Word!",
+                     idade: _req.query.idade });
 })
 
+app.put('/users/:id', (_req, _res) => {
+  return _res.json({ id: _req.params.id });
+})
+
+app.post('/users', (_req, _res) => {
+  return _res.json({ id: _req.body});
+})
+
+
 app.listen(3333);
+
+// req.query  = to filter queries
+// req.params = acess routes to update, delete...
+// req.body   = body for request use to create and edit 
