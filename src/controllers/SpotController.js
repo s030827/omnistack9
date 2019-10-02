9509d38ae6b41def6d24b149cd8ecdbe8cea1b3e
Marcus Almeida <s030827@gmail.com>
2019-10-02 17:52:39 -0300

    Validate user when create an spot

--- src/controllers/SpotController.js
+++ src/controllers/SpotController.js
@@ -1,3 +1,4 @@
+const User = require('../models/User');
 const Spot = require('../models/Spot');
 
 module.exports = {
@@ -6,6 +7,14 @@ module.exports = {
     const { company, price, techs } = _req.body;
     const { user_id }               = _req.headers;
 
+    const user = await User.findById(user_id);
+
+    if (!user) {
+
+      return _res.status(400).json({ error: "User does not exist!"})
+    
+    };
+    
     const spot = await Spot.create({
       user: user_id,
       thumbnail: filename,
