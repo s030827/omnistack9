98707c5fef5de269a7995b1810e144a44c7e693a
Marcus Almeida <s030827@gmail.com>
2019-10-06 19:09:57 -0300

    Finish return url and files

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 // import express from 'express';
 const express  = require('express');
 const mongoose = require('mongoose');
-const routes   = require('./routes');
 const cors     = require('cors');
+const path     = require('path');
+const routes   = require('./routes');
 
 require('dotenv').config()
 
@@ -16,6 +17,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
